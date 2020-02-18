b2a1509e7cc827f68db5fba88e88f9daf54e6cfc
Pawel Strojek <paulstrojek@gmail.com>
2020-02-18 14:59:54 +0100

    Add svg logo

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,25 @@
 </script>
 
 <main>
-  <h1 class="text-center">PearChat</h1>
+  <div class="logo">
+    <img src="pear.svg" alt="Pear">
+  </div>
+  <h1 class="heading">PearChat</h1>
   <Chat />
 </main>
+
+<style>
+  .logo {
+    text-align: center;
+    margin-top: 20px;
+  }
+
+  .logo img {
+    max-width: 100px;
+  }
+
+  .heading {
+    text-align: center;
+    margin-bottom: 20px;
+  }
+</style>
