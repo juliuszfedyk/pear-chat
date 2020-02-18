52fb4dea0550e7a0ce030950e198f9278870446f
Pawel Strojek <paulstrojek@gmail.com>
2020-02-18 14:24:50 +0100

    Change side menu styles and text

--- src/SideMenu.svelte
+++ src/SideMenu.svelte
@@ -1,11 +1,16 @@
 <div class="side-menu">
-  <button class="btn btn-primary">Genarate token</button>
-  <button class="btn btn-primary">Connect</button>
+  <button class="btn btn-primary">Create chat</button>
+  <button class="btn btn-primary">Connect to chat</button>
 </div>
 
 <style>
   .side-menu {
     padding: 8px;
+  }
+
+  button {
+    display: block;
+    width: 100%;
   }
 </style>
 
