c537d56cadefbe9d7678c72de09db513659ff7fb
Juliusz Fedyk <juliusz.fedyk@gmail.com>
2020-02-18 15:26:02 +0100

    feat(connection)

--- src/Chat.svelte
+++ src/Chat.svelte
@@ -2,7 +2,41 @@
   import SendMessage from "./SendMessage.svelte";
   import MessageList from "./MessageList.svelte";
   import SideMenu from "./SideMenu.svelte";
+  import { onMount } from "svelte";
+
+  let id = "";
+  let partnerId = "";
+  let peer;
+  let connection;
+
+  const connect = () => {
+    connection = peer.connect(partnerId);
+  };
+  const closeConnection = () => {
+    connection.close();
+    connection = null;
+  };
+
+  onMount(() => {
+    peer = new Peer();
+    peer.on("open", newId => {
+      id = newId;
+    });
+    peer.on("connection", newConnection => {
+      connection = newConnection;
+      connection.on('open', () => {
+      })
+      partnerId = connection.peer;
+    });
+  });
 </script>
+
+<style>
+  .main-window {
+    border: 1px solid #e2e2e2;
+    padding: 8px;
+  }
+</style>
 
 <div class="container">
   <div class="row">
@@ -11,14 +45,12 @@
       <SendMessage />
     </div>
     <div class="col-12 col-md-4">
-      <SideMenu />
+      <SideMenu
+        id={id}
+        bind:partnerId
+        on:connect={connect}
+        on:closeConnection={closeConnection}
+        connected={!!connection} />
     </div>
   </div>
 </div>
-
-<style>
-  .main-window {
-    border: 1px solid #e2e2e2;
-    padding: 8px;
-  }
-</style>
--- src/SideMenu.svelte
+++ src/SideMenu.svelte
@@ -1,7 +1,10 @@
-<div class="side-menu">
-  <button class="btn btn-primary">Genarate token</button>
-  <button class="btn btn-primary">Connect</button>
-</div>
+<script>
+  import { createEventDispatcher } from "svelte";
+  export let id;
+  export let partnerId;
+  export let connected;
+  const dispatch = createEventDispatcher();
+</script>
 
 <style>
   .side-menu {
@@ -9,3 +12,28 @@
   }
 </style>
 
+<div class="side-menu">
+  <h2>
+    <span class="badge badge-secondary">{id}</span>
+  </h2>
+  {#if !connected}
+    <input
+      type="text"
+      bind:value={partnerId}
+      class="form-control"
+      id="partner_id" />
+    <button
+      class="btn btn-primary"
+      on:click={() => dispatch('connect')}
+      disabled={!partnerId}>
+      Connect
+    </button>
+  {:else}
+    <h2>
+      <span class="badge badge-secondary">{partnerId}</span>
+    </h2>
+    <button class="btn btn-primary" on:click={() => dispatch('closeConnection')}>
+      Disconnect
+    </button>
+  {/if}
+</div>
