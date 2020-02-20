3f3b45a6e620c04453ca36836b7b44a9761e65db
Juliusz Fedyk <juliusz.fedyk@gmail.com>
2020-02-20 12:55:06 +0100

    peer service

--- src/Chat.svelte
+++ src/Chat.svelte
@@ -4,88 +4,25 @@
   import SideMenu from "./SideMenu.svelte";
   import VideoChat from "./VideoChat.svelte";
   import peerConfig from "../config.json";
-  import { peerStore } from "../peer.store.js";
-
+  import { getPeerService } from "./peer.service.js";
   import { onMount } from "svelte";
-
-  peerStore.connect();
 
   const debugMode = true;
   let id;
-  let partnerId = "";
+  let peerId = "";
   let peer;
   let connection;
   let messages = [];
+  let connectedToServer = false;
+  let connectedToPeer = false;
+  const peerService = getPeerService();
 
-  function connectToServer() {
-    debugMsg("peerId:", id);
-    peer = new Peer(id, peerConfig);
-    peer.on("error", error => {
-      addMessage(error, "admin");
-    });
-    peer.on("open", newId => {
-      debugMsg("connection to server opened, id is", newId);
-      id = newId;
-    });
-    peer.on("connection", newConnection => {
-      debugMsg("got connection from server", newConnection);
-
-      newConnection.on("open", () => {
-        debugMsg("incoming connection 'open'", newConnection);
-        setConnection(newConnection);
-        addMessage(`${partnerId} has joined`, "admin");
-      });
-    });
-  }
-
-  function disconnectFromServer() {
-    peer.disconnect();
-    peer = null;
-  }
-
-  function debugMsg(msg, obj) {
-    if (debugMode) {
-      console.log(msg, obj);
-      addMessage(msg, "admin");
-    }
-  }
-
-  function connectToPeer() {
-    const connection = peer.connect(partnerId);
-    debugMsg("requested connection", connection);
-    connection.on("open", () => {
-      debugMsg("outgoing connection open", connection);
-      setConnection(connection);
-    });
-  }
-  const disconnectFromPeer = () => {
-    connection.close();
-  };
-
-  function addMessage(text, type) {
-    messages = [...messages, { text, type }];
-  }
-
-  function setConnection(newConnection) {
-    debugMsg("setting up connection", newConnection);
-    partnerId = newConnection.peer;
-    connection = newConnection;
-    connection.on("data", text => {
-      addMessage(text, "incoming");
-    });
-    connection.on("close", () => {
-      addMessage("connection closed", "admin");
-      connection = null;
-    });
-    connection.on("error", error => {
-      addMessage(error, "admin");
-    });
-  }
-
-  function handleNewMessage(event) {
-    addMessage(event.detail.text, "outgoing");
-    connection.send(event.detail.text);
-  }
+  peerService.id.subscribe(newId => (id = newId));
+  peerService.peerId.subscribe(newPeerId => (peerId = newPeerId));
+  peerService.server.subscribe(server => (connectedToServer = !!server));
+  peerService.dataConnection.subscribe(
+    dataConnection => (connectedToPeer = !!dataConnection)
+  );
 </script>
 
 <style>
@@ -99,24 +36,25 @@
   <div class="row">
     <div class="col-12 col-md-8 main-window">
       <MessageList {messages} />
-      <SendMessage on:message={handleNewMessage} />
+      <SendMessage
+        on:message={event => peerService.sendMessage(event.detail.text)} />
     </div>
     <div class="col-12 col-md-4">
       <SideMenu
         bind:id
-        bind:partnerId
-        on:connectToServer={connectToServer}
-        on:connectToPeer={connectToPeer}
-        on:disconnectFromPeer={disconnectFromPeer}
-        connectedToPeer={!!connection}
-        connectedToServer={!!peer}
-        on:disconnectFromServer={disconnectFromServer} />
+        bind:peerId
+        on:connectToServer={peerService.connectToServer(id)}
+        on:connectToPeer={peerService.connectToPeer(peerId)}
+        on:disconnectFromPeer={peerService.disconnectFromPeer(peerId)}
+        {connectedToPeer}
+        {connectedToServer}
+        on:disconnectFromServer={peerService.disconnectFromServer()} />
     </div>
   </div>
   <div class="row">
     <div class="col-12">
       {#if peer}
-        <VideoChat {peer} {partnerId} />
+        <VideoChat {peer} {peerId} />
       {:else}
         <div>Please connect to see video chat</div>
       {/if}
